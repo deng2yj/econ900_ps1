8a6327aea3169c767bf6f5cb86a947c4066334f0
邓瑜jie <dengyj118@gmail.com>
2019-04-03 13:56:33 -0400

    try to scrape one page

--- boardgamegeek_request.py
+++ boardgamegeek_request.py
@@ -4,3 +4,12 @@ import os
 if not os.path.exists("html_files"):
 	os.mkdir("html_files")
 
+f = open("html_files/boardgamegeek.html","wb")
+response = urllib.request.urlopen("https://boardgamegeek.com/browse/boardgame")
+html = response.read()
+
+f.write(html)
+f.close()
+
+print(html)
+
