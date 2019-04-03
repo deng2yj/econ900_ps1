ddb3c258d570d458fc30a3ef3f78965b304a5b6b
邓瑜jie <dengyj118@gmail.com>
2019-04-03 14:03:55 -0400

    try too request 3 pages

--- boardgamegeek_request.py
+++ boardgamegeek_request.py
@@ -4,12 +4,16 @@ import os
 if not os.path.exists("html_files"):
 	os.mkdir("html_files")
 
-f = open("html_files/boardgamegeek.html","wb")
-response = urllib.request.urlopen("https://boardgamegeek.com/browse/boardgame")
-html = response.read()
 
-f.write(html)
-f.close()
+for i in range(3):
+	print("page:" + str(i))
+	f = open("html_files/boardgamegeek.html"+str(i)+".html","wb")
+	response = urllib.request.urlopen("https://boardgamegeek.com/browse/boardgame/page/"+str(i))
+	html = response.read()
 
-print(html)
+	f.write(html)
+	f.close()
+
+#print(html)
+	print("requesting boardgamegeek")
 
