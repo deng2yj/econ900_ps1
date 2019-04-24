4a7b08455b82cada644f3be42d16135496ef8da6
邓瑜jie <dengyj118@gmail.com>
2019-04-23 22:22:14 -0400

    scrape all rows and use regular expressions to get listed prices

--- boardgamegeek_parse.py
+++ boardgamegeek_parse.py
@@ -1,6 +1,7 @@
 import os
 from bs4 import BeautifulSoup
 import glob
+import re
 # import pandas as pd
 
 if not os.path.exists("parsed_results"):
@@ -21,27 +22,42 @@ f.close()
 
 boardgames_table = soup.find("table", {"id": "collectionitems"})
 # print(boardgames_table)
-# boardgames_tbody = boardgames_table.find("tbody")
+boardgames_tbody = boardgames_table.find("tbody")
 # print(boardgames_tbody)
-boardgames_rows = boardgames_table.find_all("tr", {"id":"row_"})
+boardgames_rows = boardgames_tbody.find_all("tr", {"id":"row_"})
 # print(boardgames_rows)
 
+for r in boardgames_rows:
 
-boardgame_name = boardgames_rows[0].find("td", {"class": "collection_objectname"}).find("a").text
-boardgame_rank = boardgames_rows[0].find("td", {"class": "collection_rank"}).text
-boardgame_year = boardgames_rows[0].find("td", {"class": "collection_objectname"}).find("span").text
-boardgame_geekrating = boardgames_rows[0].find("td", {"class": "collection_bggrating"}).text
-boardgame_avgrating = boardgames_rows[0].find("td", {"class": "collection_bggrating"}).findNext("td", {"class": "collection_bggrating"}).text
-boardgame_voters = boardgames_rows[0].find("td", {"class": "collection_bggrating"}).findNext("td", {"class": "collection_bggrating"}).findNext("td", {"class": "collection_bggrating"}).text
-boardgame_price = boardgames_rows[0].find("td", {"collection_shop"}).find("div", {"class": "aad"}).find("div").find("div").text
-# collection_Listprice = boardgame_price.next_element.next_element.next_element.next_element
-print(boardgame_year)
-print(boardgame_name)
-print(boardgame_rank)
-print(boardgame_geekrating)
-print(boardgame_avgrating)
-print(boardgame_voters)
-print(boardgame_price)
+	boardgame_name = r.find("td", {"class": "collection_objectname"}).find("a").text
+	boardgame_rank = r.find("td", {"class": "collection_rank"}).text
+	boardgame_year = r.find("td", {"class": "collection_objectname"}).find("span").text.replace("(","").replace(")","")
+	boardgame_geekrating = r.find("td", {"class": "collection_bggrating"}).text
+	boardgame_avgrating = r.find("td", {"class": "collection_bggrating"}).findNext("td", {"class": "collection_bggrating"}).text
+	boardgame_voters = r.find("td", {"class": "collection_bggrating"}).findNext("td", {"class": "collection_bggrating"}).findNext("td", {"class": "collection_bggrating"}).text
+	boardgame_price = r.find("td", {"collection_shop"}).find("div", {"class": "aad"})
+	# print(boardgame_price.text)
+	boardgame_list = re.search(r'List:\s\$\d+\.\d+', boardgame_price.text)
+	# 'List: \$\d+\.\d+'
+	
+	
+	if boardgame_list is None:
+			continue
+	else:
+			boardgame_listprice = boardgame_list.group(0).replace("List:","").replace("$","")
+			# boardgame_listpricef = boardgame_listprice.replace("List:","").replace("$","")
+
+	# print(boardgame_listprice)
+		# print(boardgame_price.text)
+		# boardgame_listprice = boardgame_price.find("div").find("div").replace("List:", "")
+
+	print("boardgame_year: " + boardgame_year)
+	print("boardgame_name: " + boardgame_name)
+	print("boardgame_rank: " + boardgame_rank)
+	print("boardgame_geekrating: " + boardgame_geekrating)
+	print("boardgame_avgrating: " + boardgame_avgrating)
+	print("boardgame_voters: " + boardgame_voters)
+	print("boardgame_listprice: " + boardgame_listprice)
 
 
 
